8a2d7edc845398997ef66b0357ae4aadc8527364
villanicole <201910772@gordoncollege.edu.ph>
2022-03-29 23:25:37 +0800

    Exam

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title,'home')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <h1> Welcome User! </h1>
+        </div>
+    </div>
+</div>
+        
+@endsection
